3eb434a97c517dcd8b82780e11f5d3de84417ac0
Владимиров Александр <kvipka90@yandex.ru>
2025-03-07 20:15:56 +0300

    added time delay for @Timed

--- src/main/java/org/acme/service/GreetingService.java
+++ src/main/java/org/acme/service/GreetingService.java
@@ -4,12 +4,20 @@ import io.micrometer.core.annotation.Counted;
 import io.micrometer.core.annotation.Timed;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @ApplicationScoped
 public class GreetingService {
 
     @Timed(value = "greetings.service.method.hello.time", longTask = true, extraTags = {"extraFlag1", "extraFlag2"})
     @Counted(value = "greetings.service.method.hello.count", extraTags = {"extraFlag1", "extraFlag2"})
     public String getHello() {
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 5 + 1);
+        try {
+            Thread.sleep(randomNum * 1000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
         return "Hello from Quarkus REST";
     }
 }
